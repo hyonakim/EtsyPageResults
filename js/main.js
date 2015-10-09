d77483813c2345c0a09cc124ba78c2967c2c063c
Hyona Kim <tekkieninja@gmail.com>
2015-10-09 13:32:29 -0400

    Added etsydata images and description in JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 (function () {
 
-  console.log('It Works!');
+ var templateString = $('#itemTemplate').text();
+
+ var templateFunction = _.template(templateString);
+
+ _.each(etsydata.results, function (item) {
+  var itemHTML = templateFunction(item);
+  $('.container').append(itemHTML);
+
+ });
 
 }());
