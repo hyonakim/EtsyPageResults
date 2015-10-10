51421e418ba9b1900cf51e55d89ca57468d7c90f
Hyona Kim <tekkieninja@gmail.com>
2015-10-10 00:46:39 -0400

    Added heart and hamburger icons to item lists

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@
  });
 
 }());
+
+(function () {
+
+$('.container').on('mouseenter', '.itemList', function (event) {
+var show = $(this).find('.icons').addClass('.show-icons');
+show.css('display', 'inline');
+});
+
+$('.container').on('mouseleave', '.itemList', function (event) {
+var noShow = $(this).find('.icons').removeClass('.show-icons');
+noShow.css('display', 'none');
+});
+
+}());
